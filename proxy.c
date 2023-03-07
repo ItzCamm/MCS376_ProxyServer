0ca6e6f2e387be2b95b1f6cc2bb1c3c216bc4464
Cam <cameronjboelter@gmail.com>
2023-03-07 11:54:49 -0600

    proxy server

--- proxy.c
+++ proxy.c
@@ -1,14 +1,74 @@
 #include <stdio.h>
 
-/* Recommended max cache and object sizes */
-#define MAX_CACHE_SIZE 1049000
-#define MAX_OBJECT_SIZE 102400
-
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 
-int main()
+int main(int argc, char **argv) 
 {
-    printf("%s", user_agent_hdr);
-    return 0;
+    int listenfd, connfd;
+    char hostname[MAXLINE], port[MAXLINE];
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+
+    /* Check command line args */
+    if (argc != 2) {
+    fprintf(stderr, "usage: %s <port>\n", argv[0]);
+    exit(1);
+    }
+
+    listenfd = Open_listenfd(argv[1]);
+    while (1) {
+    clientlen = sizeof(clientaddr);
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); //line:netp:tiny:accept
+        Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE, 
+                    port, MAXLINE, 0);
+        printf("Accepted connection from (%s, %s)\n", hostname, port);
+    doit(connfd);                                             //line:netp:tiny:doit
+    Close(connfd);                                            //line:netp:tiny:close
+    }
+}
+
+/*handle the client HTTP transaction*/
+void doit(int connfd)
+{
+    int end_serverfd;/*the end server file descriptor*/
+
+    char buf[MAXLINE],method[MAXLINE],uri[MAXLINE],version[MAXLINE];
+    char endserver_http_header [MAXLINE];
+    /*store the request line arguments*/
+    char hostname[MAXLINE],path[MAXLINE];
+    int port;
+
+    rio_t rio,server_rio;/*rio is client's rio,server_rio is endserver's rio*/
+
+    Rio_readinitb(&rio,connfd);
+    Rio_readlineb(&rio,buf,MAXLINE);
+    sscanf(buf,"%s %s %s",method,uri,version); /*read the client request line*/
+
+    if(strcasecmp(method,"GET")){
+        printf("Proxy does not implement the method");
+        return;
+    }
+    
+    /*parse the uri to get hostname,file path ,port*/
+    /*WRITE YOUR OWN*/
+
+    /*build the http header which will send to the end server*/
+    /*WRITE YOUR OWN*/
+
+    /*connect to the end server*/
+    /*WRITE YOUR OWN*/
+
+    Rio_readinitb(&server_rio,end_serverfd);
+    /*write the http header to endserver*/
+    Rio_writen(end_serverfd,endserver_http_header,strlen(endserver_http_header));
+
+    /*receive message from end server and send to the client*/
+    size_t n;
+    while((n=Rio_readlineb(&server_rio,buf,MAXLINE))!=0)
+    {
+        printf("proxy received %d bytes,then send\n",n);
+        Rio_writen(connfd,buf,n);
+    }
+    Close(end_serverfd);
 }
